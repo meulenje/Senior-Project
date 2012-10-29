fe4156c68803365aeba1897a2bec2c505418045b
Austin <delamaau@mail.gvsu.edu>
2012-10-29 19:27:00 -0400

    Creating Abstract Object to hold the 4 basic types of grid objects. (terrain, building, entity, item)
    exit

---
+++ Building.java
@@ -0,0 +1,18 @@
+package rpg;
+
+/**
+ * Building class
+ * 
+ * 
+ * @author Austin
+ * @version 10/28/2012
+ */
+public class Building {
+
+	private GameEngine GE;
+	
+	public Building(GameEngine tempEngine)
+	{
+		GE = tempEngine;
+	}
+}
---
+++ Terrain.java
@@ -0,0 +1,18 @@
+package rpg;
+
+/**
+ * Terrain class
+ * 
+ * 
+ * @author Austin
+ * @version 10/28/2012
+ */
+public class Terrain extends RPGObject {
+
+	private GameEngine GE;
+	
+	public Terrain(GameEngine tempEngine)
+	{
+		GE = tempEngine;
+	}
+}
